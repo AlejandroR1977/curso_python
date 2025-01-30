64891d58596106b421ac7968d6f746bf1ed9a61c
AlejandroR1977 <111005527+AlejandroR1977@users.noreply.github.com>
2025-01-29 18:02:47 -0700

    tablero corregido

--- gato/tablero.py
+++ gato/tablero.py
@@ -5,14 +5,17 @@ def dibuja_tablero(simbolos:dict):
     '''
     Dibuja tablero del juego de el gato
     '''
-    print('tablero')
-    # print(f'''
-    #       {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
-    #   ----------
-    #       {simbolos['4']} | {simbolos['5']} | {simbolos['6']}
-    #   ----------
-    #       {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
-    #     ''')
+    for i in range(1,10):
+        if str(i) not in simbolos:
+            simbolos[str(i)] = ''
+
+    print(f'''
+          {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
+      ----------------
+          {simbolos['4']} | {simbolos['5']} | {simbolos['6']}
+      ----------------
+          {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
+        ''')
     
 if __name__ == '__main__':
     numeros = [str(x) for x in range (1,10)]
