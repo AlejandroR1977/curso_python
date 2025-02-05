c23c1c49035b277bdd58702e74b92384c8ab07c7
AlejandroR1977 <111005527+AlejandroR1977@users.noreply.github.com>
2025-02-05 10:50:25 -0700

    App llamando a tablero integrado

---
+++ gato/app.py
@@ -0,0 +1,19 @@
+'''
+este archi es el punto de entrada de la aplicación
+'''
+import tablero
+
+def main():
+    '''
+    Funion principal
+    '''
+    numeros = [str(i) for i in range (1,10)]
+    dsimbolos = {x: x for x in numeros}
+    g= tablero.juego(dsimbolos)
+    if g is not None:
+        print (f'El ganador es {g}')
+    else:
+        print ('Empate')
+
+if __name__ == '__main__':
+    main()
